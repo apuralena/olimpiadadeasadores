ea8df965d4187dc1b7abb3a27cb3b5211c68774f
Lucas-Andres-GF <lucas.gallardo196304@alumnos.info.unlp.edu.ar>
2025-08-23 20:21:59 -0300

    Add @astrojs/vercel dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,19 @@ import { defineConfig } from "astro/config"; // 👈 DESCOMENTAR
 import tailwind from "@tailwindcss/vite"; // 👈 DESCOMENTAR
 import react from "@astrojs/react"; // 👈 DESCOMENTAR
 
+import vercel from "@astrojs/vercel";
+
 export default defineConfig({
   vite: { plugins: [tailwind()] },
   integrations: [react()],
+
   server: {
     host: true, // accesible por IP local
     port: 4321, // (opcional) fuerza 4321; si está ocupado, cambialo
   },
-  output: "server", // endpoints POST disponibles sin prerender
+
+  // endpoints POST disponibles sin prerender
+  output: "server",
+
+  adapter: vercel()
 });
